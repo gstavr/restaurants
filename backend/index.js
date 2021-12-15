53e6a17eceb132d26e04d2748facdf96d9b0ad34
Giorgos Stavrou <g.stavrou@epsilonet.gr>
2021-12-15 23:32:12 +0200

    Fixe

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const MongoClient = mongodb.MongoClient;
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
   maxPoolSize: 50,
